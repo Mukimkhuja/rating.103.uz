db7d6dbe6130909ed23cdf4d3acf147243f1199a
mukimkhuja <mr.muqumxuja@mail.ru>
2020-05-06 23:46:22 +0500

    rating v1.3

--- resources/views/enter/docrating.blade.php
+++ resources/views/enter/docrating.blade.php
@@ -1,4 +1,25 @@
 @extends('layouts.app')
 @section('app_title')
     Тиббиёт ходимларини баҳолаш
-    @endsection
+@endsection
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                <!-- <div class="card-header">Ассалому алайкум, <?php echo Auth::user()->name."! "; ?> </div> -->
+
+                    <div class="card-body2">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                            Тиббиёт ходимларини баҳолаш
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/enter/jobrating.blade.php
+++ resources/views/enter/jobrating.blade.php
@@ -2,3 +2,24 @@
 @section('app_title')
     Фаолиятни баҳолаш
 @endsection
+
+@section('content')
+    <div class="container-md">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <!-- <div class="card-header">Ассалому алайкум, <?php echo Auth::user()->name."! "; ?> </div> -->
+
+                    <div class="card-body2">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        Фаолият баҳоси
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endsection
