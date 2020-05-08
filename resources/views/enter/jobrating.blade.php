d27f1b27119bb28cf32e32fb438046374e51f381
Администратор <https://github.com/Mukimkhuja/rating.103.uz.git>
2020-05-08 12:11:53 +0500

    rating v1.5 comp2

--- resources/views/enter/jobrating.blade.php
+++ resources/views/enter/jobrating.blade.php
@@ -16,55 +16,57 @@
                                 {{ session('status') }}
                             </div>
                         @endif
-                        Фаолият баҳосини аниқлаш учун киритилиши лозим бўлган маълумотлар:
+                            <div class="text-lg-center font-italic font-weight-bolder">
+                                <p>Фаолият баҳосини аниқлаш учун киритилиши лозим бўлган маълумотлар:</p>
+                            </div>
 
                             <div class="pre-scrollable">
                                 <div class="form-group">
-                                    <label for="npipl" >N(pipl) - баҳоланаётган ТТЁ хизмати таркибий бўлинмасига бириктирилган ҳудуддаги аҳоли сони</label>
+                                    <label for="npipl" class="form-control-range">N(pipl) - баҳоланаётган ТТЁ хизмати таркибий бўлинмасига бириктирилган ҳудуддаги аҳоли сони</label>
                                     <input name="npipl" id="npipl" type="number" class="form-control-sm" placeholder="Enter the number!!!">
                                 </div>
                                 <div class="form-group">
-                                    <label for="nfabc" >Nf(ABC) – Таҳлил даврида хизматда бўлган махсус автомобиллар сони</label>
+                                    <label for="nfabc" class="form-control-range">Nf(ABC) – Таҳлил даврида хизматда бўлган махсус автомобиллар сони</label>
                                     <input name="nfabc" id="nfabc" type="number" class="form-control-sm" placeholder="Enter the number!!!">
                                 </div>
                                 <div class="form-group">
-                                    <label for="nfbc" >Nf(BC) – Таҳлил даврида хизматда бўлган Реанимобил (HUNDAY) типидаги автомобиллар сони</label>
+                                    <label for="nfbc" class="form-control-range">Nf(BC) – Таҳлил даврида хизматда бўлган Реанимобил (HUNDAY) типидаги автомобиллар сони</label>
                                     <input name="nfbc" id="nfbc" type="number" class="form-control-sm" placeholder="Enter the number!!!">
                                 </div>
                                 <div class="form-group">
-                                    <label for="nabc" >Na(B+C) - Таҳлил даврида хизматда бўлган  мобил+планшетлар сони</label>
+                                    <label for="nabc" class="form-control-range">Na(B+C) - Таҳлил даврида хизматда бўлган  мобил+планшетлар сони</label>
                                     <input name="nabc" id="nabc" type="number" class="form-control-sm" placeholder="Enter the number!!!">
                                 </div>
                                 <div class="form-group">
-                                    <label for="nfgps" >Nf(GPS) - Таҳлил даврида бор бўлган GPS*трекерлар сони </label>
+                                    <label for="nfgps" class="form-control-range">Nf(GPS) - Таҳлил даврида бор бўлган GPS*трекерлар сони </label>
                                     <input name="nfgps" id="nfgps" type="number" class="form-control-sm" placeholder="Enter the number!!!">
                                 </div>
                                 <div class="form-group">
-                                    <label for="nfvr" >Nf(vr)-банд врач ставкалар сони</label>
+                                    <label for="nfvr" class="form-control-range">Nf(vr)-банд врач ставкалар сони</label>
                                     <input name="nfvr" id="nfvr" type="number" class="form-control-sm" placeholder="Enter the number!!!">
                                 </div>
                                 <div class="form-group">
-                                    <label for="npvr" >Np(vr)- штат нормативи бўйича врач ставкалари сони</label>
+                                    <label for="npvr" class="form-control-range">Np(vr)- штат нормативи бўйича врач ставкалари сони</label>
                                     <input name="npvr" id="npvr" type="number" class="form-control-sm" placeholder="Enter the number!!!">
                                 </div>
                                 <div class="form-group">
-                                    <label for="nfpara" >Nf(para)-банд парамедик ставкалари сони</label>
+                                    <label for="nfpara" class="form-control-range">Nf(para)-банд парамедик ставкалари сони</label>
                                     <input name="nfpara" id="nfpara" type="number" class="form-control-sm" placeholder="Enter the number!!!">
                                 </div>
                                 <div class="form-group">
-                                    <label for="npipl" >N(pipl) - баҳоланаётган ТТЁ хизмати таркибий бўлинмасига бириктирилган ҳудуддаги аҳоли сони</label>
+                                    <label for="nppara" class="form-control-range">Np(para)- штат нормативи бўйича парамедик ставкалари сони</label>
+                                    <input name="nppara" id="nppara" type="number" class="form-control-sm" placeholder="Enter the number!!!">
+                                </div>
+                                <div class="form-group">
+                                    <label for="npipl" class="form-control-range">N(pipl) - баҳоланаётган ТТЁ хизмати таркибий бўлинмасига бириктирилган ҳудуддаги аҳоли сони</label>
                                     <input name="npipl" id="npipl" type="number" class="form-control-sm" placeholder="Enter the number!!!">
                                 </div>
                                 <div class="form-group">
-                                    <label for="npipl" >N(pipl) - баҳоланаётган ТТЁ хизмати таркибий бўлинмасига бириктирилган ҳудуддаги аҳоли сони</label>
+                                    <label for="npipl" class="form-control-range">N(pipl) - баҳоланаётган ТТЁ хизмати таркибий бўлинмасига бириктирилган ҳудуддаги аҳоли сони</label>
                                     <input name="npipl" id="npipl" type="number" class="form-control-sm" placeholder="Enter the number!!!">
                                 </div>
                                 <div class="form-group">
-                                    <label for="npipl" >N(pipl) - баҳоланаётган ТТЁ хизмати таркибий бўлинмасига бириктирилган ҳудуддаги аҳоли сони</label>
-                                    <input name="npipl" id="npipl" type="number" class="form-control-sm" placeholder="Enter the number!!!">
-                                </div>
-                                <div class="form-group">
-                                    <label for="npipl" >N(pipl) - баҳоланаётган ТТЁ хизмати таркибий бўлинмасига бириктирилган ҳудуддаги аҳоли сони</label>
+                                    <label for="npipl" class="form-control-range">N(pipl) - баҳоланаётган ТТЁ хизмати таркибий бўлинмасига бириктирилган ҳудуддаги аҳоли сони</label>
                                     <input name="npipl" id="npipl" type="number" class="form-control-sm" placeholder="Enter the number!!!">
                                 </div>
                             </div>
